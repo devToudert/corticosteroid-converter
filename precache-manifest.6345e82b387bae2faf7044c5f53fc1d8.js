8f6e74c0e076e4df5d7bd60040bb345858164457
devtoudert <dev.toudert@gmail.com>
2021-10-23 18:26:28 +0100

    Updates

---
+++ precache-manifest.6345e82b387bae2faf7044c5f53fc1d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb925d431296158834a5f1e69eb83823",
+    "url": "/corticosteroid-converter/index.html"
+  },
+  {
+    "revision": "1fb7c0e8350335cd4f08",
+    "url": "/corticosteroid-converter/static/css/main.5f81ac75.chunk.css"
+  },
+  {
+    "revision": "5656c14796253634783d",
+    "url": "/corticosteroid-converter/static/js/2.418ad80d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/corticosteroid-converter/static/js/2.418ad80d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fb7c0e8350335cd4f08",
+    "url": "/corticosteroid-converter/static/js/main.5aa13e0e.chunk.js"
+  },
+  {
+    "revision": "5007cffb2ef0a760b18c",
+    "url": "/corticosteroid-converter/static/js/runtime-main.019ea9c2.js"
+  }
+]);
